c7b2dc71bb97f681dc5917a5914968ce6c0f4be2
Alejandro Alzate <alejoalzate12@gmail.com>
2024-05-27 12:16:17 -0500

    Add leaflet-rails gem for mapping support and implement search functionality on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
+    @properties = Property.all
+
+    if params[:query].present?
+      @properties = @properties.where("address ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    end
+
+    if params[:property_type].present?
+      @properties = @properties.where(property_type: params[:property_type])
+    end
+
+    if params[:min_price].present? && params[:max_price].present?
+      @properties = @properties.where(price: params[:min_price]..params[:max_price])
+    end
+
+    if params[:bedrooms].present?
+      @properties = @properties.where(bedrooms: params[:bedrooms])
+    end
+
+    if params[:bathrooms].present?
+      @properties = @properties.where(bathrooms: params[:bathrooms])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :properties
   devise_for :users
   
+  root to: 'home#index'
+
 end
