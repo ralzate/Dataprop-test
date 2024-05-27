e0d8d734925b19dee3c9e30ae181b81c06ff7bad
Alejandro Alzate <alejoalzate12@gmail.com>
2024-05-26 23:27:46 -0500

    Add views, controller, and routes for properties model

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,59 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # GET /properties
+  def index
+    @properties = Property.all
+  end
+
+  # GET /properties/1
+  def show
+  end
+
+  # GET /properties/new
+  def new
+    @property = Property.new
+  end
+
+  # GET /properties/1/edit
+  def edit
+  end
+
+  # POST /properties
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      redirect_to @property, notice: 'Property was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /properties/1
+  def update
+    if @property.update(property_params)
+      redirect_to @property, notice: 'Property was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /properties/1
+  def destroy
+    @property.destroy
+    redirect_to properties_url, notice: 'Property was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def property_params
+    params.require(:property).permit(:property_type, :price, :currency, :district, :address, :area, :bedrooms, :bathrooms, :latitude, :longitude, :description)
+  end
+end
