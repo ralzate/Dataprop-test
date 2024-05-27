84285e59f669e842ef1b24785fe44b0e92726b68
Alejandro Alzate <alejoalzate12@gmail.com>
2024-05-27 06:51:07 -0500

    Add user authentication and authorization for properties, associate properties with users, and update views and schema accordingly.

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -18,35 +18,61 @@ class PropertiesController < ApplicationController
 
   # GET /properties/1/edit
   def edit
+    unless current_user == @property.user
+      redirect_to @property, alert: "You are not authorized to edit this property."
+    end
   end
 
   # POST /properties
   def create
-    @property = Property.new(property_params)
+    @property = current_user.properties.build(property_params)
 
-    if @property.save
-      redirect_to @property, notice: 'Property was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @property.save
+        format.html { redirect_to property_url(@property), notice: "Property was successfully created." }
+        format.json { render :show, status: :created, location: @property }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /properties/1
   def update
-    if @property.update(property_params)
-      redirect_to @property, notice: 'Property was successfully updated.'
-    else
-      render :edit
+    unless current_user == @property.user
+      redirect_to @property, alert: "You are not authorized to update this property."
+      return
+    end
+
+    respond_to do |format|
+      if @property.update(property_params)
+        format.html { redirect_to property_url(@property), notice: "Property was successfully updated." }
+        format.json { render :show, status: :ok, location: @property }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /properties/1
   def destroy
-    @property.destroy
-    redirect_to properties_url, notice: 'Property was successfully destroyed.'
+    unless current_user == @property.user
+      redirect_to properties_url, alert: "You are not authorized to delete this property."
+      return
+    end
+
+    @property.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to properties_url, notice: "Property was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_property
     @property = Property.find(params[:id])
